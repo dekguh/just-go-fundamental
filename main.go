b3b9ff16c5fa07aee4a8d552858fccb58a8aae26
just kadek <kdkteguhmahesa@gmail.com>
2025-08-24 22:08:51 +0800

    Add tests and update main.go functionality

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"learn/just-go-fundamental/helpers"
 )
 
+func Hello(name string) string {
+	return name
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(helpers.TextCapitalize("hello world"))
