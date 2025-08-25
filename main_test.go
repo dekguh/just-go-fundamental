a72b1892d7ccfe70258ec5f9ae261fac3aa26595
just kadek <kdkteguhmahesa@gmail.com>
2025-08-25 14:52:38 +0800

    Update test file with additional test cases

--- main_test.go
+++ main_test.go
@@ -3,10 +3,12 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("world")
-	want := "hello"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("world")
+		want := "hello"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
