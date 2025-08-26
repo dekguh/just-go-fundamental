a7d0b1e38f8acb3aafb6eeb5af70834ac505f32c
just kadek <kdkteguhmahesa@gmail.com>
2025-08-26 10:03:09 +0800

    Update main.go functionality

--- main.go
+++ main.go
@@ -14,4 +14,37 @@ func main() {
 	fmt.Println(helpers.TextCapitalize("hello world"))
 	fmt.Println(helpers.InterpolateFormula(1, 10, 20))
 	fmt.Println(helpers.GenerateCarDetail())
+
+	var balance int = 0
+	var wallet *int = &balance
+	*wallet = 20
+	fmt.Println(balance)
+
+	totalFruit := map[string]int{
+		"apple":      10,
+		"banana":     20,
+		"cherry":     30,
+		"date":       40,
+		"elderberry": 50,
+	}
+	fmt.Println(totalFruit)
+	totalFruit["watermelon"] = 999
+	fmt.Println(totalFruit)
+
+	type User struct {
+		ID    int
+		name  string
+		email string
+	}
+
+	var users = make(map[int]User)
+	users[1] = User{
+		ID:    1,
+		name:  "John Doe",
+		email: "john.doe@example.com",
+	}
+
+	for _, user := range users {
+		fmt.Println(user)
+	}
 }
